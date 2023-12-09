b04ca786eafa4a30b4738a344a734680c0b3de20
brunobarreira <brunobarreirasilva@gmail.com>
2023-12-08 23:44:25 -0300

    test:integration: teste jest-dynamo

--- tests/integration/some.test.ts
+++ tests/integration/some.test.ts
@@ -1,7 +1,27 @@
-import { describe, test, expect } from '@jest/globals'
+import { describe, test, expect, beforeAll } from '@jest/globals'
+import { makeDatabaseConnection } from './makeDatabaseConnection'
+import { DynamoDB } from '@aws-sdk/client-dynamodb'
+import { FileModel } from './File'
 
 describe('[INTEGRATION TEST]', () => {
-  test('ensure it sum two numbers', () => {
-    expect(4+5).toBe(3)
+  let ddb: void
+
+  beforeAll(() => {
+    ddb = makeDatabaseConnection()
+  })
+
+  test('teste jest-dynamo', async () => {
+    try {
+      await FileModel.create({
+        id: '1',
+        hello: 'world'
+      })
+      console.log("item added successfully!")
+      const allItems = await FileModel.scan().exec();
+      console.log("[allItems]: ", allItems)
+    } catch (error) {
+      console.log("[ERROR]: ", error)
+      return
+    }
   })
 })
